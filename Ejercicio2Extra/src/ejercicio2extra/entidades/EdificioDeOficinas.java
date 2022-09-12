d40a99baf7a185531d9bf1780f8cd7c13e3cbca7
NicoBerenstein <99055210+NicoBerenstein@users.noreply.github.com>
2022-09-12 19:54:33 -0300

    Update EdificioDeOficinas.java

--- Ejercicio2Extra/src/ejercicio2extra/entidades/EdificioDeOficinas.java
+++ Ejercicio2Extra/src/ejercicio2extra/entidades/EdificioDeOficinas.java
@@ -1,10 +1,3 @@
-/*Clase EdificioDeOficinas con sus atributos número de oficinas, cantidad de personas
-por oficina y número de pisos. Esta clase implementará los dos métodos abstractos y
-los atributos del padre.
-De esta clase nos interesa saber cuántas personas pueden trabajar en todo el edificio, el
-usuario dirá cuántas personas entran en cada oficina, cuantas oficinas y el número de piso
-(suponiendo que en cada piso hay una oficina). Crear el método cantPersonas(), que muestre
-cuantas personas entrarían en un piso y cuantas en todo el edificio.*/
 package ejercicio2extra.entidades;
 
 
